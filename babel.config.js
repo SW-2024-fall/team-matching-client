5e2739c0fc2d101d4833a3f7441f341503122cff
김혜주 <sueelly@naver.com>
2024-10-05 06:12:39 +0900

    Fix: babel 절대경로 수정 및 추가

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,28 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = [
-    'module-resolver',
-    {
-      root: ['./src'],
-      extensions: [
-        '.ios.js',
-        '.android.js',
-        '.js',
-        '.ios.jsx',
-        '.android.jsx',
-        '.jsx',
-        '.jsx',
-        '.js',
-        '.json',
-      ],
-    },
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@layout': './src/layout',
+          '@pages': './src/pages',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+          '@navigation': './src/navigation',
+        },
+        extensions: ['.ios.js', '.android.js', '.js', '.ios.jsx', '.android.jsx', '.jsx', '.json'],
+      },
+    ],
   ];
 
   return {
     presets: ['babel-preset-expo'],
+    plugins,
   };
 };
