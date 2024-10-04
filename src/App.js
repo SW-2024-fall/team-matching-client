3a1dd161e5ca57d94a7309160d8f3ec55b9b9e23
김혜주 <sueelly@naver.com>
2024-10-05 00:50:40 +0900

    Feat: themeStyle 적용(Font, color, border)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from './styles/ThemeStyles';
+import styled, { ThemeProvider } from 'styled-components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>테스트 테스트2</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>테스트 테스트2</Text>
+        <StatusBar style="auto" />
+      </View>
+      <Container>
+        <Text>themeprovider test</Text>
+      </Container>
+    </ThemeProvider>
   );
 }
+
+const Container = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.blue.primary};
+`;
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/styles/ThemeStyles.js
@@ -0,0 +1,76 @@
+const greyColors = {
+  100: '#F9FAFB',
+  200: '#E5E8EB',
+  400: '#b0b8c1',
+  500: '#8B95A1',
+  700: '#4E5968',
+  800: '#333D48',
+  900: '#191f28',
+};
+
+const greyBlueColors = {
+  100: '#B9D9EB',
+  200: '#9BCBEB',
+  400: '#0077C8',
+  500: '#005EB8',
+  600: '#004094',
+  700: '#002D63',
+};
+
+const blueColors = {
+  100: '#B9D9EB',
+  200: '#9BCBEB',
+  400: '#0098FF',
+  500: '#0082FF',
+  600: '#0066EB',
+  700: '#0059C3',
+};
+
+export const theme = {
+  font: {
+    color: {
+      light: greyColors[400],
+      primary: greyColors[800],
+    },
+    size: {
+      xSmall: '10px',
+      small: '12px',
+      primary: '14px',
+      large: '18px',
+      xLarge: '28px',
+    },
+    weight: {
+      light: 400,
+      regular: 400,
+      primary: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+      extraBold: 800,
+    },
+  },
+  border: {
+    radius: {
+      small: '8px',
+      primary: '12px',
+      medium: '12px',
+      large: '16px',
+    },
+    color: {
+      primary: greyColors[400],
+    },
+  },
+  colors: {
+    grey: {
+      border: greyColors[400],
+      light: greyColors[200],
+      primary: greyColors[800],
+      dark: greyColors[900],
+    },
+    blue: {
+      light: blueColors[100],
+      primary: blueColors[500],
+      hover: blueColors[600],
+    },
+  },
+};
