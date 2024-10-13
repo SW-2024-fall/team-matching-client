3ddc6eda86f72b3a738271c093b2bca58dcda52e
김혜주 <sueelly@naver.com>
2024-10-14 01:45:46 +0900

    Feat: 뒤로가기 버튼 흰/검 나눔

--- src/layout/header/Components/GoBackBtn.jsx
+++ src/layout/header/Components/GoBackBtn.jsx
@@ -1,6 +1,7 @@
 import { View, Pressable } from 'react-native';
 import { WithLocalSvg } from 'react-native-svg/css';
 import backBtnIcon from '@assets/backBtnIcon.svg';
+import backBtnIconWhite from '@assets/backBtnIcon_white.svg';
 import { theme } from '@styles/ThemeStyles';
 
 export default function GoBackBtn({ canGoBack, goBack, color = theme.colors.grey.primary }) {
@@ -11,7 +12,11 @@ export default function GoBackBtn({ canGoBack, goBack, color = theme.colors.grey
           goBack();
         }}
       >
-        <WithLocalSvg color={color} asset={backBtnIcon} width={24} height={24} />
+        {color == theme.colors.grey.primary ? (
+          <WithLocalSvg color={color} asset={backBtnIcon} width={24} height={24} />
+        ) : (
+          <WithLocalSvg color={color} asset={backBtnIconWhite} width={24} height={24} />
+        )}
       </Pressable>
     );
   } else {
