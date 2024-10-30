97b426af594e7ecb83992911d9d182c9a421dec8
PF03106 <gofl4520@gmail.com>
2024-10-30 16:33:09 +0900

    modified category, meetingType, recruitmentStatus design of FilterModal

--- src/components/FilterModal.jsx
+++ src/components/FilterModal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Modal, View, Text, Button, StyleSheet, Pressable, TextInput } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import RadioButtonRN from 'radio-buttons-react-native'; // 라디오 버튼 컴포넌트
-import {theme} from '@styles/ThemeStyles'
+import { theme } from '@styles/ThemeStyles';
 
 const FilterModal = ({ visible, onClose, onApply }) => {
     const [selectedCategory, setSelectedCategory] = useState([]);
@@ -27,7 +27,6 @@ const FilterModal = ({ visible, onClose, onApply }) => {
         "게임/오락",
         "기타"
     ];
-    
 
     const toggleCategory = (category) => {
         setSelectedCategory(prev => 
@@ -77,54 +76,73 @@ const FilterModal = ({ visible, onClose, onApply }) => {
                 <View style={styles.content}>
                     {activeFilter === 'category' && (
                         <View style={styles.optionContainer}>
-                            {categories.map(category => (
-                                <View key={category} style={styles.checkboxContainer}>
-                                    <Checkbox
-                                        value={selectedCategory.includes(category)}
-                                        onValueChange={() => toggleCategory(category)}
-                                    />
-                                    <Text>{category}</Text>
-                                </View>
-                            ))}
+                            <View style={styles.checkboxGrid}>
+                                {categories.map(category => (
+                                    <View key={category} style={styles.checkboxContainer}>
+                                        <Checkbox
+                                            value={selectedCategory.includes(category)}
+                                            onValueChange={() => toggleCategory(category)}
+                                            style={styles.checkbox}
+                                        />
+                                        <Text style={styles.checkboxText}>{category}</Text>
+                                    </View>
+                                ))}
+                            </View>
                         </View>
                     )}
                     
                     {activeFilter === 'meetingType' && (
                         <View style={styles.optionContainer}>
-                            <RadioButtonRN
-                                data={[{label: '일회성'}, {label: '정기'}]}
-                                selectedBtn={(e) => setMeetingType(e.label)}
-                                initial={meetingType === '일회성' ? 1 : meetingType === '정기' ? 2 : 0}
-                            />
+                            <View style={styles.radioContainer}>
+                                <Pressable onPress={() => setMeetingType('일회성')} style={styles.radioButton}>
+                                    <View style={[styles.radioSquare, meetingType === '일회성' && styles.selectedRadio]} />
+                                    <Text style={[styles.radioText, meetingType === '일회성' && styles.selectedRadioText]}>일회성</Text>
+                                </Pressable>
+                                <Pressable onPress={() => setMeetingType('정기')} style={styles.radioButton}>
+                                    <View style={[styles.radioSquare, meetingType === '정기' && styles.selectedRadio]} />
+                                    <Text style={[styles.radioText, meetingType === '정기' && styles.selectedRadioText]}>정기</Text>
+                                </Pressable>
+                            </View>
                         </View>
                     )}
 
                     {activeFilter === 'recruitmentStatus' && (
                         <View style={styles.optionContainer}>
-                            <RadioButtonRN
-                                data={[{label: '모집 중'}, {label: '모집 완료'}]}
-                                selectedBtn={(e) => setRecruitmentStatus(e.label)}
-                                initial={recruitmentStatus === '모집 중' ? 1 : recruitmentStatus === '모집 완료' ? 2 : 0}
-                            />
+                            <View style={styles.radioContainer}>
+                                <Pressable onPress={() => setRecruitmentStatus('모집 중')} style={styles.radioButton}>
+                                    <View style={[styles.radioSquare, recruitmentStatus === '모집 중' && styles.selectedRadio]} />
+                                    <Text style={[styles.radioText, recruitmentStatus === '모집 중' && styles.selectedRadioText]}>모집 중</Text>
+                                </Pressable>
+                                <Pressable onPress={() => setRecruitmentStatus('모집 완료')} style={styles.radioButton}>
+                                    <View style={[styles.radioSquare, recruitmentStatus === '모집 완료' && styles.selectedRadio]} />
+                                    <Text style={[styles.radioText, recruitmentStatus === '모집 완료' && styles.selectedRadioText]}>모집 완료</Text>
+                                </Pressable>
+                            </View>
                         </View>
                     )}
 
+                    {/* 인원 수 디자인 변경 */}
                     {activeFilter === 'participants' && (
                         <View style={styles.optionContainer}>
-                            <TextInput
-                                keyboardType="numeric"
-                                value={minParticipants.toString()}
-                                onChangeText={text => setMinParticipants(Number(text))}
-                                style={styles.input}
-                                placeholder="최소 인원"
-                            />
-                            <TextInput
-                                keyboardType="numeric"
-                                value={maxParticipants.toString()}
-                                onChangeText={text => setMaxParticipants(Number(text))}
-                                style={styles.input}
-                                placeholder="최대 인원"
-                            />
+                            <View style={styles.participantsContainer}>
+                                <Text style={styles.participantsLabel}>최소</Text>
+                                <TextInput
+                                    keyboardType="numeric"
+                                    value={minParticipants.toString()}
+                                    onChangeText={text => setMinParticipants(Number(text))}
+                                    style={styles.participantsInput}
+                                    placeholder="2"
+                                />
+                                <Text style={styles.participantsLabel}>~</Text>
+                                <TextInput
+                                    keyboardType="numeric"
+                                    value={maxParticipants.toString()}
+                                    onChangeText={text => setMaxParticipants(Number(text))}
+                                    style={styles.participantsInput}
+                                    placeholder="99"
+                                />
+                                <Text style={styles.participantsLabel}>명</Text>
+                            </View>
                         </View>
                     )}
                 </View>
@@ -187,14 +205,66 @@ const styles = StyleSheet.create({
     optionContainer: {
         paddingVertical: 10,
     },
+    checkboxGrid: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
     checkboxContainer: {
         flexDirection: 'row',
         alignItems: 'center',
+        width: '30%', // 3열로 배열하기 위해 각 항목의 너비를 설정
         marginVertical: 5,
+    },
+    checkbox: {
+        width: 10,
+        height: 10,
+        marginRight: 4,
+        borderRadius: 0,
+        borderWidth: 1.5,
+        borderColor: theme.colors.grey.border,
+    },
+    checkboxText: {
+        color: theme.font.color.light,
+    },
+    radioContainer: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        marginVertical: 10,
+    },
+    radioButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 0,
+        padding: 10,
+    },
+    radioSquare: {
+        width: 10,
+        height: 10,
+        borderWidth: 1.5,
+        borderColor: theme.colors.grey.border,
+        backgroundColor: 'transparent',
+        marginRight: 8,
+        borderRadius: 0, // Slight rounding for a more polished square look
+    },
+    selectedRadio: {
+        backgroundColor: theme.colors.blue.primary, // Change to primary color when selected
+        borderColor: theme.colors.grey.blur,
+    },
+    radioText: {
+        color: theme.font.color.light,
+        fontSize: theme.font.size.small,
+    },
+    selectedRadioText:{
+        color: theme.font.color.primary,
+    },
+    participantsContainer:{
+        borderRadius: 4,
+        borderColor: theme.border.color,
     },
     input: {
         borderBottomWidth: 1,
-        borderBottomColor: '#ddd',
+        borderBottomColor: theme.colors.grey.border,
         marginVertical: 10,
         padding: 10,
     },
@@ -202,8 +272,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         paddingTop: 20,
-        borderRadius: theme.border.primary,
-        backgroundColor: theme.colors.blue.primary,
+
     },
 });
 
