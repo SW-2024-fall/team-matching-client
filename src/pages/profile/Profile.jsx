9dceaed59b69c40f502dc579b2418b9835be71aa
PF03106 <gofl4520@gmail.com>
2024-10-30 19:28:37 +0900

    Feat: Add Profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,14 +1,10 @@
 import { PAGES } from '@navigation/constant';
-import Layout from '@layout/layout';
-import { Text } from 'react-native';
+import MyProfile from './MyProfile';
+import PublicProfile from './PublicProfile';
 
 export default function Profile({ route }) {
   const screen = route.name;
-  const isMe = screen == PAGES.MYPROFILE;
+  const isMe = screen === PAGES.MYPROFILE;
 
-  return (
-    <Layout screen={screen}>
-      <Text>{isMe ? '내 프로필' : '상대방 프로필  '}</Text>
-    </Layout>
-  );
+  return isMe ? <MyProfile /> : <PublicProfile />;
 }
