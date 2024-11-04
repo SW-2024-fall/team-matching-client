1b3b9b77e6b38aeff63f233beff47d1b37370c3b
jaearam <jaearam@naver.com>
2024-11-04 15:42:42 +0900

    LOGINPAGE LAYOUT APPLIED

---
+++ src/pages/auth/login/components/Input.jsx
@@ -0,0 +1,26 @@
+// src/components/Input.js
+import React from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+
+const Input = ({ placeholder, value, onChangeText }) => {
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      value={value}
+      onChangeText={onChangeText}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 5,
+  },
+});
+
+export default Input;
