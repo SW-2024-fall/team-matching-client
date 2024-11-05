d0d89a98130b157ce882a71248bf986a3a2f9514
jaearam <jaearam@naver.com>
2024-11-05 11:35:31 +0900

    App.js test cmd dlt

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { ThemeProvider } from 'styled-components';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Navigation from '@navigation/navigation';
-import MeetingHistoryCreate from './pages/meetingHistory/create/MeetingHistoryCreate';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +10,6 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Navigation />
-      <MeetingHistoryCreate />
     </ThemeProvider>
   );
 }
