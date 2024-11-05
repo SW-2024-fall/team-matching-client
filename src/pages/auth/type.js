960cd013e0975cfa0f96ecf2c9b84bdb14497ae3
PF03106 <gofl4520@gmail.com>
2024-11-05 23:14:19 +0900

    feat: add signup at type.js

--- src/pages/auth/type.js
+++ src/pages/auth/type.js
@@ -2,3 +2,4 @@ export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const AUTO_LOGIN = 'AUTO_LOGIN';
 export const REFRESH_TOKEN = 'REFRESH_TOKEN';
+export const SING_UP = 'SING_UP';
