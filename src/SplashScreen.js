8820e41c266b8b0dc64d7a36e812131628564996
PF03106 <gofl4520@gmail.com>
2024-11-06 00:06:38 +0900

    fix: SplashScreen.jsx

--- src/SplashScreen.js
+++ src/SplashScreen.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Splash from '.@pages/auth/containers/Splash'
 import { View} from 'react-native';
+import {UserContext} from '@context/userContext';
 
 function SplashScreen({ navigation }) {
+    const { isLoggedIn } = useContext(UserContext);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (isLoggedIn) {
+                navigation.replace('Main');
+            } else {
+                navigation.replace('Auth');
+            }
+        }, 3000);
+    }, [isLoggedIn]);
+
     return (
         <View >
             <Splash navigation={navigation} />
