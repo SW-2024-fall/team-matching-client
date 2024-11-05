8ec4652249fc4b816ba8e4d5923f745db8fc0541
PF03106 <gofl4520@gmail.com>
2024-11-05 23:53:45 +0900

    fix: Apps.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import SplashScreen from './SplashScreen';
 import Login from '@pages/login/Login';//구현된 페이지에서 가져오기
 import Register from '@pages/register/Register';//구현된 페이지에서 가져오기
 import Navigation from '@navigation/navigation';
+import { UserProvider } from './context/userContext';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Navigation>
-        <Stack.Navigator initialRouteName="Splash">
-          <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Login" component={AuthScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="Register" component={MainScreen} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </Navigation>
+      <UserProvider>
+        <Navigation>
+          <Stack.Navigator initialRouteName="Splash">
+            <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Login" component={AuthScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={MainScreen} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </Navigation>
+      </UserProvider>
     </ThemeProvider>
   );
 }
