3620af67c76fc7ddcadf4035bd4dfb542a915bc1
PF03106 <gofl4520@gmail.com>
2024-11-05 23:42:55 +0900

    feat: App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { theme } from './styles/ThemeStyles';
 import { ThemeProvider } from 'styled-components';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import SplashScreen from './SplashScreen';
+import Login from '@pages/login/Login';
+import Register from '@pages/register/Register';
 import Navigation from '@navigation/navigation';
 
 const Stack = createNativeStackNavigator();
@@ -9,7 +11,13 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Navigation />
+      <Navigation>
+        <Stack.Navigator initialRouteName="Splash">
+          <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Login" component={AuthScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Register" component={MainScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </Navigation>
     </ThemeProvider>
   );
 }
