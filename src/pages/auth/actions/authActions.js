f485dd9e823f07fe68af25cfcdd625e09bc03654
PF03106 <gofl4520@gmail.com>
2024-11-05 22:44:30 +0900

    feat: authActions.js

---
+++ src/pages/auth/actions/authActions.js
@@ -0,0 +1,21 @@
+import * as types from '../types';
+import AsyncStorage from '@react-native-community/async-storage';
+import { request } from './api'; // request 함수는 API 요청을 보낼 때 사용할 유틸리티 함수로 가정
+
+export function login(dataToSubmit) {
+    const data = request("POST", "/login", dataToSubmit);
+    return {
+        type: types.LOGIN,
+        payload: data,
+        uniqueId: dataToSubmit.id
+    };
+}
+
+export function logout() {
+    AsyncStorage.multiRemove(['accessToken', 'refreshToken', 'uniqueId']);
+    return { type: types.LOGOUT };
+}
+
+export function autoLogin(data) {
+    return { type: types.AUTO_LOGIN, payload: data };
+}
