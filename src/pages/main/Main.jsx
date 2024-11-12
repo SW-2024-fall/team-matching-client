d516ee56d4b9ea215e46a4b8c17bff7f85431435
PF03106 <gofl4520@gmail.com>
2024-11-12 23:45:46 +0900

    fix: address

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList , Image, ScrollView} from 'react-native';
+import { View, Text, StyleSheet,  Image, ScrollView, ActivityIndicator} from 'react-native';
 import { PAGES } from '@navigation/constant';
 import Layout from '@layout/layout';
 import { theme } from '@styles/ThemeStyles';
@@ -10,20 +10,21 @@ import { useEffect, useState } from 'react';
 
 
 export default function Main({ navigation }) {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     
     useEffect(() => {
         const fetchData = async () => {
             try {
-            const response = await fetch("http://10.0.84.8:8080/api/meetings",{method:"GET"});
+            const response = await fetch("http://10.0.73.4:8080/api/meetings",{method:"GET"});
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             const json = await response.json(); 
             setData(json);
             setLoading(false);
+            console.log(json); // 반환된 JSON 구조 확인
             } catch (error) {
             setError(error.message);
             } finally {
@@ -35,19 +36,26 @@ export default function Main({ navigation }) {
         fetchData();
     }, []);
 
+    if (loading) {
+        return <ActivityIndicator size="large" color="#000000" />; // 로딩 중일 때 인디케이터 표시
+    }
 
     return (
         <Layout screen={PAGES.MAIN} navigation={navigation}>
-            {/* Story Section */}
+            {/* Story Section: 모임 활동에 관한 것 */}
             <View style={styles.storyContainer}>
                 <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.storyList}>
                     {data.data.map((item) => (
-                        <StoryCircle key={item.userId} imageUrl={item.imageUrl} userId={item.userId} />
+                        <StoryCircle 
+                        key={item.userId} 
+                        imageUrl={item.imageUrl} 
+                        userId={item.userId} 
+                        />
                     ))}
                 </ScrollView>
             </View>
             
-            {/* Feed Section */}
+            {/* Feed Section: 모임장 이름 + 그 사람이 작성한 활동  */}
             <View style={styles.feedPostContainer}>
                 <ScrollView>
                     {data.data.map((item) => (
