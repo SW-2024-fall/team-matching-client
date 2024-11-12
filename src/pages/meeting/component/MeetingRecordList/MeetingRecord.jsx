51ed2e96e16b30505c07f5d75b6eae6e899776b4
JAEWON1414 <saint1414@naver.com>
2024-11-12 11:11:57 +0900

    FEAT: meeting histroy API

--- src/pages/meeting/component/MeetingRecordList/MeetingRecord.jsx
+++ src/pages/meeting/component/MeetingRecordList/MeetingRecord.jsx
@@ -1,51 +1,66 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, TouchableWithoutFeedback } from "react-native";
 import styled from "styled-components";
 import profile1 from '../../../../assets/profileExample1.svg';
 import { WithLocalSvg } from "react-native-svg/css";
 import menu from '../../../../assets/menu.svg';
 import runningPhoto from '../../../../assets/runningPhoto.svg';
 import useModal from "../../../../hooks/useModal";
+import { useState } from "react";
 
 export default function MeetingRecord({ name, group, content, historyId }) {
     const previewText = content.length > 100 ? `${content.substring(0, 100)}...` : content;
     const { Modal, open, close } = useModal();
+    
     const onPressDelete = async () => {
-        const url = `/api/histories/${historyId}`;
-        const data = {
-            targetUserId: { userId }
-        };
         try {
-            const response = await fetch(url, {
-                method: 'DELETE',
-                body: JSON.stringify(data)
-            });
-            if (response.ok) {
-                const responseBody = await response.json();
-                console.log("응답 데이터:", responseBody);
-            } else {
-                console.error("요청 실패:", response.status);
-            }
-        } catch (error) {
-            console.error("네트워크 오류:", error);
-        }
+            const response = await fetch(`http://192.168.219.101:8080/api/histories/${historyId}`, { method: "DELETE" });
+            if (!response.ok) { throw new Error("Failed to 히스토리 삭제"); }
+        } catch (error) { console.error("Error 히스토리 삭제:", error); }
     };
     const onPressUpdate = async () => {
         //히스토리 수정 페이지 이동
     };
+    const [menuVisible, setMenuVisible] = useState(false);
+    const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
+    const handlePress = (event) => {
+        const { pageX, pageY } = event.nativeEvent;
+        setMenuPosition({ x: pageX, y: pageY });
+        setMenuVisible(!menuVisible);
+    };
+    const closeMenu = () => {
+        if (menuVisible) {
+            setMenuVisible(false);
+        }
+    };
     return (
-        <Container >
-            <Header>
-                <HeaderLeft>
-                    <WithLocalSvg asset={profile1} />
-                    <HeaderNameContainer>
-                        <Name>{name}</Name>
-                        <MeetingName >{group}</MeetingName>
-                    </HeaderNameContainer>
-                </HeaderLeft>
-                <Pressable onPress={open}>
-                    <WithLocalSvg asset={menu} />
-                </Pressable>
-                <Modal>
+        <TouchableWithoutFeedback onPress={closeMenu}>
+            <Container >
+
+
+                <Header>
+                    <HeaderLeft>
+                        <WithLocalSvg asset={profile1} />
+                        <HeaderNameContainer>
+                            <Name>{name}</Name>
+                            <MeetingName >{group}</MeetingName>
+                        </HeaderNameContainer>
+                    </HeaderLeft>
+                    {!menuVisible && <Pressable onPress={handlePress}>
+                        <WithLocalSvg asset={menu} />
+                    </Pressable>}
+
+                    {menuVisible && (
+                        <MenuContainer>
+                            <Pressable onPress={onPressDelete}>
+                                <MenuText>삭제</MenuText>
+                            </Pressable>
+                            <Line></Line>
+                            <Pressable onPress={onPressUpdate}>
+                                <MenuText>수정</MenuText>
+                            </Pressable>
+                        </MenuContainer>
+                    )}
+                    {/* <Modal>
                     <ModalView>
                         <Pressable onPress={onPressDelete}>
                             <Text>삭제</Text>
@@ -55,19 +70,36 @@ export default function MeetingRecord({ name, group, content, historyId }) {
                             <Text>수정</Text>
                         </Pressable>
                     </ModalView>
-                </Modal>
-            </Header>
-            <PhotoWrapper>
-                <WithLocalSvg asset={runningPhoto} />
-            </PhotoWrapper>
+                </Modal> */}
+                </Header>
+                <PhotoWrapper>
+                    <WithLocalSvg asset={runningPhoto} />
+                </PhotoWrapper>
 
-            <Content numberOfLines={3}>{previewText}</Content>
-        </Container>
+                <Content numberOfLines={3}>{previewText}</Content>
+            </Container>
+        </TouchableWithoutFeedback>
     )
 }
 const Line = styled.View`
   borderBottomWidth:1px;
   width:100%;
+`;
+const MenuContainer = styled.View`
+    border:1px;
+    borderRadius:8px;
+    width:55px;
+    height:40px;
+    justifyContent:center;
+    alignItems:center;
+    paddingTop:3px;
+    paddingBottom:3px;
+`;
+const MenuText = styled.Text`
+    fontWeight:${(props) => props.theme.font.weight.regular};
+    fontSize:${(props) => props.theme.font.size.small};
+    color:${(props) => props.theme.font.color.primary};
+    
 `;
 const ModalView = styled.View`
     position:relative;
--- src/pages/meeting/component/MeetingRecordList/MeetingRecordList.jsx
+++ src/pages/meeting/component/MeetingRecordList/MeetingRecordList.jsx
@@ -3,60 +3,6 @@ import MeetingRecord from "./MeetingRecord";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 
-const MeetingHistoryResponse = {
-    code: "SUCCESS",
-    message: "string",
-    data: [
-        {
-            id: 0,
-            thumbnailUrl: "string",
-            content: "오늘 React Native 배웠어요.오늘 React Native 배웠어요.오늘 React Native 배웠어요.오늘 React Native 배웠어요.오늘 React Native 배웠어요.오늘 React Native 배웠어요.오늘 React Native 배웠어요.오늘 React Native 배웠어요.",
-            writer: {
-                id: "string",
-                name: "홍길동",
-                profileUrl: "string",
-                attendenceScore: 0,
-                major: "PUBLIC_ADMINISTRATION",
-                studentId: "string",
-                phoneNumber: "string",
-                features: [
-                    "string"
-                ]
-            },
-            meetingId: 0,
-            meetingName: "React Native 스터디"
-        },
-        {
-            id: 1,
-            thumbnailUrl: "string",
-            content: "오늘 독서 모임 정말 좋았어요! 오늘 독서 모임 정말 좋았어요!오늘 독서 모임 정말 좋았어요!오늘 독서 모임 정말 좋았어요!오늘 독서 모임 정말 좋았어요!오늘 독서 모임 정말 좋았어요!오늘 독서 모임 정말 좋았어요!",
-            writer: {
-                id: "string",
-                name: "최재원",
-                profileUrl: "string",
-                attendenceScore: 0,
-                major: "PUBLIC_ADMINISTRATION",
-                studentId: "string",
-                phoneNumber: "string",
-                features: [
-                    "string"
-                ]
-            },
-            meetingId: 0,
-            meetingName: "독서 모임(이름 예시)"
-        }
-    ],
-    page: {
-        number: 0,
-        size: 0,
-        totalCount: 0,
-        hasNext: true,
-        hasPrevious: true
-    }
-}
-
-const meetingHistoryData = MeetingHistoryResponse.data;
-
 export default function MeetingRecordList({id}) {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -70,7 +16,7 @@ export default function MeetingRecordList({id}) {
                 }
                 const json = await response.json();
                 setData(json.data);
-                console.log("Data = "+data.data);
+                console.log("Data = "+JSON.stringify(data));
                 setLoading(false);
             } catch (error) {
                 setError(error.message);
@@ -90,17 +36,17 @@ export default function MeetingRecordList({id}) {
     }
     return (
         <Container>
-            {/* <View>
-                {meetingHistoryData.map((item) => (
+            <View>
+                {data.map((item) => (
                     <MeetingRecord
                         key={item.id}
                         name={item.writer.name}
                         group={item.meetingName}
                         content={item.content}
-                        historyId={item.historyId}
+                        historyId={item.id}
                     />
                 ))}
-            </View> */}
+            </View>
         </Container>
     )
 }
