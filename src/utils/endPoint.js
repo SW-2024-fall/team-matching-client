324e0c586ca7500cb2f92c00c4c3b3d5c6616ba5
김혜주 <sueelly@naver.com>
2024-12-08 02:28:57 +0900

    change endpoints

--- src/utils/endPoint.js
+++ src/utils/endPoint.js
@@ -2,9 +2,9 @@ export const HOST = "http://localhost:8080";
 
 export const API_URL = `${HOST}/api`;
 export const AUTH = `${API_URL}/auth`;
-export const USER = `${API_URL}/user`;
-export const MEETING = `${API_URL}/meeting`;
-export const MEETING_HISTORY = `${API_URL}/history`;
+export const USER = `${API_URL}/users`;
+export const MEETING = `${API_URL}/meetings`;
+export const MEETING_HISTORY = `${API_URL}/histories`;
 export const SCRAP = `${API_URL}/scrap`;
 export const LIKE = `${API_URL}/like`;
 export const COMMENT = `${API_URL}/comment`;
