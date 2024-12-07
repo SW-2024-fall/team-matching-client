01cfd22a683e414101c8728c3a028244bd96fb43
김혜주 <sueelly@naver.com>
2024-12-08 02:09:09 +0900

    feat(utils): Extract translateMajorToEnglish to utils folder

---
+++ src/utils/translateMajorToEnglish.js
@@ -0,0 +1,9 @@
+export function translateMajorToEnglish(koreanName) {
+    const entries = Object.entries(Major);
+    for (const [englishName, name] of entries) {
+        if (name === koreanName) {
+            return englishName;
+        }
+    }
+    return null; // 이름이 없을 경우 null 반환
+}
